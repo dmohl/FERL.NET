d0169d507a42992291e084479e139ab174f754a8
Dan Mohl <dmohl@yahoo.com>
2010-05-09 18:03:15 -0500

    Continued working on the connection creation functionality

--- src/FERL.NET/Node.fs
+++ src/FERL.NET/Node.fs
@@ -4,6 +4,8 @@ open System.Net
 open System.Net.Sockets
 open NodeCommon
 
+let EpmdPort = 4369
+
 let BuildPeerNode nodeName =
     let assignedNodeName = BuildNodeName nodeName
     let pid = CreatePid assignedNodeName 0
@@ -18,6 +20,33 @@ let BuildSelfNode nodeName erlangCookie port =
     let assignedNodeName = BuildNodeName nodeName
     let pid = CreatePid assignedNodeName assignedPort
     {NodeName = assignedNodeName; TcpListener = tcpListener; Pid = pid; Port = assignedPort; Cookie = erlangCookie}
-   
+
+let GetNodeNameParts (nodeName:string) =
+    let nodeNameParts = nodeName.Split('@')
+    nodeNameParts.[0], nodeNameParts.[1]
+
+let LookupPortInEmpd node =
+    let nodeShortName, nodeHostName = GetNodeNameParts node.NodeName
+    let epmdTcpClient = new TcpClient(nodeHostName, EpmdPort)
+    let epmdResponseStream = epmdTcpClient.GetStream()
+    //epmdResponseStream.ReadByte()
+//				int response = ibuf.read1();
+//				if (response == port4resp)
+//				{
+//					int result = ibuf.read1();
+//					if (result == 0)
+//					{
+//						port = ibuf.read2BE();
+//						
+//						node.ntype = ibuf.read1();
+//						node._proto = ibuf.read1();
+//						node._distHigh = ibuf.read2BE();
+//						node._distLow = ibuf.read2BE();
+//						// ignore rest of fields
+//					}
+//				}
+    0
+
+
 let ConnectNodes selfNode peerNode = 
     {SelfNode = selfNode; PeerNode = peerNode; IsConnected = true}
