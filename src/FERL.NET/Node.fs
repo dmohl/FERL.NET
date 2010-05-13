c17b4e55d5fff5cc23e02189b1e62003f29aa1a6
Dan Mohl <dmohl@yahoo.com>
2010-05-13 11:51:05 -0500

    Continued adding node connection functionality

--- src/FERL.NET/Node.fs
+++ src/FERL.NET/Node.fs
@@ -1,10 +1,13 @@
 ﻿module Node
 
+open System
 open System.Net
 open System.Net.Sockets
 open NodeCommon
 
 let EpmdPort = 4369
+let PortForRequest = 122
+let PortForResponse = 119
 
 let BuildPeerNode nodeName =
     let assignedNodeName = BuildNodeName nodeName
@@ -25,12 +28,28 @@ let GetNodeNameParts (nodeName:string) =
     let nodeNameParts = nodeName.Split('@')
     nodeNameParts.[0], nodeNameParts.[1]
 
-let LookupPortInEmpd node =
+let LookupNodeInformationInEmpd node =
     let nodeShortName, nodeHostName = GetNodeNameParts node.NodeName
     let epmdTcpClient = new TcpClient(nodeHostName, EpmdPort)
     let epmdResponseStream = epmdTcpClient.GetStream()
-    //epmdResponseStream.ReadByte()
-//				int response = ibuf.read1();
+    let response = epmdResponseStream.ReadByte()
+    match response with
+    | _ when response = PortForResponse ->
+        let result = epmdResponseStream.ReadByte()
+        let rec ReadPortFromStream (stream:NetworkStream) (port:string) =
+            match port.Length with
+            | length when length < 5 -> Int32.Parse(port)
+            | _ ->
+                let newPort = port + stream.ReadByte().ToString() 
+                ReadPortFromStream stream newPort
+        let port = ReadPortFromStream epmdResponseStream ""
+//		let ntype = epmdResponseStream.ReadByte()
+//		let proto = epmdResponseStream.ReadByte()
+//		let distHigh = //ibuf.read2BE();
+//		let distLow = //ibuf.read2BE();
+        {NodeName = node.NodeName; TcpListener = node.TcpListener; Pid = node.Pid; Port = port; Cookie = node.Cookie}
+    | _ -> node
+
 //				if (response == port4resp)
 //				{
 //					int result = ibuf.read1();
@@ -45,8 +64,6 @@ let LookupPortInEmpd node =
 //						// ignore rest of fields
 //					}
 //				}
-    0
-
 
 let ConnectNodes selfNode peerNode = 
     {SelfNode = selfNode; PeerNode = peerNode; IsConnected = true}
